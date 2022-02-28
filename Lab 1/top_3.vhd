7ff3c1fea3eaf100d4e39bc3a95e8da57f4976cb
swood76 <35699946+swood76@users.noreply.github.com>
2022-02-27 23:08:57 -0500

    Pretty sure Part 3 done and complete
    Modified logic, added wires for each bit in sequence to be detected

--- Lab 1/top_3.vhd
+++ Lab 1/top_3.vhd
@@ -3,64 +3,72 @@ use ieee.std_logic_1164.all;
 
 entity top_3 is
 port(
-	reset:in std_logic;
-	clock:in std_logic;
-	w:in std_logic;
-	
-	z:out std_logic);
+reset:in std_logic;
+clock:in std_logic;
+w:in std_logic;
+
+z:out std_logic);
 
 end entity;
 
 
 
 architecture rtl of top_3 is
-	component parallel_shift_si
-		 port(
+component parallel_shift_si
+port(
  clk : in std_logic;
- rst : in std_logic;
+ --rst : in std_logic;
  D: in std_logic;
  Q: out std_logic_vector(3 downto 0));
 end component;
-	
-	
-	signal Y_Q:std_logic_vector(3 downto 0);
-	signal Y_D:std_logic;
-	
-	
-	signal nand_1:std_logic;
-	signal nand_2:std_logic;
 
-	
-		
+
+signal Y_Q_1:std_logic_vector(3 downto 0);
+signal Y_Q_0:std_logic_vector(3 downto 0);
+signal Y_D:std_logic;
+signal ones:std_logic;
+signal zeros:std_logic;
+
+
+
 
 begin
-	
-	
-		
-	
 
-		
 
-	
 
-	
+
+
+
+
+
+
+
 ff0:parallel_shift_si
-	port map(
-		clk => clock,
-		rst => reset,
-		d => w,
-		q => Y_Q);
+port map(
+clk => clock,
 
-	
+d => w,
+q => Y_Q_0);
 
-		
-	nand_1 <= (Y_Q(0) or Y_Q(1));
-	
-	nand_2 <= (Y_Q(2) or Y_Q(3));
-	
-	
-	z <= not  (nand_1 and nand_2);
-	
-	
+--rset_reg <= not reset;
+ff1:parallel_shift_si
+port map(
+clk => clock,
+d => w,
+q => Y_Q_1);
+
+zeros <= not (Y_Q_0(0) or Y_Q_0(1) or Y_Q_0(2) or Y_Q_0(3));
+
+--nand_2 <= ;
+
+
+ones <= Y_Q_1(0) and Y_Q_1(1) and  Y_Q_1(2) and Y_Q_1(3);
+--and_2 <= );
+--not(  (nand_1 and nand_2) ) or
+
+z <=  ones or zeros;
+
+
 
 end rtl;
+
